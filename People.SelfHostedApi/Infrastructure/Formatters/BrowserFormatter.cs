db33c62249988f3b3d206f2b4a833a952640ac1c
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-09-10 22:00:15 +0100

    Added BrowserFormatter to format response in JSON for browsers

---
+++ People.SelfHostedApi/Infrastructure/Formatters/BrowserFormatter.cs
@@ -0,0 +1,22 @@
+﻿namespace People.SelfHostedApi.Infrastructure.Formatters
+{
+    using System;
+    using System.Net.Http.Formatting;
+    using System.Net.Http.Headers;
+    using Newtonsoft.Json;
+
+    public class BrowserFormatter : JsonMediaTypeFormatter
+    {
+        public BrowserFormatter()
+        {
+            SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            SerializerSettings.Formatting = Formatting.Indented;
+        }
+
+        public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+        {
+            base.SetDefaultContentHeaders(type, headers, mediaType);
+            headers.ContentType = new MediaTypeHeaderValue("application/json");
+        }
+    }
+}
