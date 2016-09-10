974f2fb45a00b3a222525b1a3dcc79fb140d0a6d
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-09-10 21:58:19 +0100

    Added configuration for web api routes

---
+++ People.SelfHostedApi/App_Start/WebApiRouteConfig.cs
@@ -0,0 +1,17 @@
+﻿namespace People.SelfHostedApi
+{
+    using System.Web.Http;
+    using Controllers;
+    using Infrastructure.Formatters;
+
+    public class WebApiRouteConfig
+    {
+        public static void Register(HttpConfiguration configuration)
+        {
+            configuration.Formatters.Add(new BrowserFormatter());
+
+            configuration.MapHttpAttributeRoutes();
+            configuration.Routes.MapHttpRoute("Default", "api/{controller}/{id}", new { controller = nameof(PeopleController), id = RouteParameter.Optional });
+        }
+    }
+}
