97616429103864d05518f6b1f0ef88448fa98c1c
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-09-10 21:58:40 +0100

    Added PeopleController to fetch people resources from database

--- People.SelfHostedApi/Controllers/PeopleController.cs
+++ People.SelfHostedApi/Controllers/PeopleController.cs
@@ -1,13 +1,28 @@
 ﻿namespace People.SelfHostedApi.Controllers
 {
+    using System.Linq;
     using System.Web.Http;
+    using Database;
 
-    [Authorize]
-    public class PeopleController: ApiController
+    public class PeopleController : ApiController
     {
+        private readonly ApplicationDbContext _context;
+
+        public PeopleController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         public IHttpActionResult Get()
         {
-            return Ok("people");
+            var people = _context.People.ToList();
+            return Ok(people);
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            var person = _context.People.Find(id);
+            return Ok(person);
         }
     }
 }
