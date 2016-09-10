2cf5577298ec5d7329e3ded09f9f42db5ac7602a
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-09-10 21:59:22 +0100

    Added UserDetailsController to fetch details for authenticated user

---
+++ People.SelfHostedApi/Controllers/UserDetailsController.cs
@@ -0,0 +1,22 @@
+﻿namespace People.SelfHostedApi.Controllers
+{
+    using System.Net.Http;
+    using System.Web.Http;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.Owin;
+
+    [Authorize]
+    public class UserDetailsController : ApiController
+    {
+        private ApplicationUserManager _userManager;
+        public ApplicationUserManager UserManager => 
+            _userManager ?? (_userManager = Request.GetOwinContext().GetUserManager<ApplicationUserManager>());
+
+        public IHttpActionResult Get()
+        {
+            var id = User.Identity.GetUserId();
+            var user = UserManager.FindById(id);
+            return Ok(user);
+        }
+    }
+}
