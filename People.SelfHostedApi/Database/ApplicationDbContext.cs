984641c5d076fd224c4bd985a3e14860464a1ff9
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-09-10 21:59:40 +0100

    Added Identity dbcontext

---
+++ People.SelfHostedApi/Database/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace People.SelfHostedApi.Database
+{
+    using System.Data.Entity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
+
+    public class ApplicationDbContext: IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(): base("DefaultConnection", throwIfV1Schema: false) { }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}
