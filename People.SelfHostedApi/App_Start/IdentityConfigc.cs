25d5b64222b1d589607b38ffa6f91618e9352aea
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-09-10 21:58:06 +0100

    Added configuration for ASP.NET Identity

---
+++ People.SelfHostedApi/App_Start/IdentityConfigc.cs
@@ -0,0 +1,22 @@
+﻿namespace People.SelfHostedApi
+{
+    using Database;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Microsoft.AspNet.Identity.Owin;
+    using Microsoft.Owin;
+
+    public class ApplicationUserManager : UserManager<IdentityUser>
+    {
+        public ApplicationUserManager(IUserStore<IdentityUser> store) : base(store)
+        {
+        }
+
+        public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options,
+            IOwinContext context)
+        {
+            var dbContext = context.Get<ApplicationDbContext>();
+            return new ApplicationUserManager(new UserStore<IdentityUser>(dbContext));
+        }
+    }
+}
