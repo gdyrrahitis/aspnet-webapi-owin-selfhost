1573dc9cacdaf54f720c2e17af7804eeaf8c5b03
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-10-05 00:47:33 +0100

    Updated services as per unit test

---
+++ People.Services/Person/IPersonService.cs
@@ -0,0 +1,10 @@
+﻿namespace People.Services.Person
+{
+    using System.Collections.Generic;
+    using Domain.Entities;
+
+    public interface IPersonService
+    {
+        IEnumerable<Person> GetPeople();
+    }
+}
---
+++ People.Services/Person/PersonService.cs
@@ -0,0 +1,22 @@
+﻿namespace People.Services.Person
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Domain.Entities;
+    using Domain.Repository;
+
+    public class PersonService: IPersonService
+    {
+        private readonly IRepository<Person, string> _repository;
+
+        public PersonService(IRepository<Person, string> repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<Person> GetPeople()
+        {
+            return _repository.All.ToList();
+        }
+    }
+}
