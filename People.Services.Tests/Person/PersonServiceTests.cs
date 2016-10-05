93396f806bb2002328d8046c12fd88931670a04f
GeorgeD <GeorgeD@CUBIC-1112>
2016-10-05 17:05:36 +0100

    Added unit tests for persons service

--- People.Services.Tests/Person/PersonServiceTests.cs
+++ People.Services.Tests/Person/PersonServiceTests.cs
@@ -11,12 +11,12 @@
     [TestFixture]
     public class PersonServiceTests
     {
-        private Mock<IRepository<Person, string>> _repositoryMock;
+        private Mock<IRepository<Person, int>> _repositoryMock;
 
         [SetUp]
         public void Setup()
         {
-            _repositoryMock = new Mock<IRepository<Person, string>>();
+            _repositoryMock = new Mock<IRepository<Person, int>>();
         }
 
         [Test]
@@ -57,6 +57,89 @@
             // Assert
             Assert.IsTrue(result.Any());
             Assert.AreEqual(people.Count, result.Count());
+        }
+
+        [Test]
+        public void GetPerson_ReturnsPersonEntity_Test()
+        {
+            // Arrange
+            const int invalidId = 5;
+            const int id = 4;
+            const string name = "George";
+            const int age = 26;
+            var person = new Person
+            {
+                Id = id,
+                Name = name,
+                Age = age
+            };
+            _repositoryMock.Setup(m => m.Find(It.Is<int>(i => i == id))).Returns(person);
+            _repositoryMock.Setup(m => m.Find(It.Is<int>(i => i == invalidId))).Returns(() => null);
+            var service = new PersonService(_repositoryMock.Object);
+
+            // Act
+            var result = service.GetPerson(id);
+            var invalidResult = service.GetPerson(invalidId);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsNull(invalidResult);
+            Assert.IsInstanceOf<Person>(result);
+            Assert.AreEqual(id, result.Id);
+            Assert.AreEqual(name, result.Name);
+            Assert.AreEqual(age, result.Age);
+        }
+
+        [Test]
+        public void Update_ShouldCallRepositoryUpdateOnce_Test()
+        {
+            // Arrange
+            const int id = 4;
+            const string name = "George";
+            const int age = 26;
+            var person = new Person
+            {
+                Id = id,
+                Name = name,
+                Age = age
+            };
+            var invalidPerson = new Person();
+            _repositoryMock.Setup(m => m.Update(It.Is<Person>(p => p == person)))
+                .Verifiable();
+            var service = new PersonService(_repositoryMock.Object);
+
+            // Act
+            service.Update(person);
+
+            // Assert
+            _repositoryMock.Verify(m => m.Update(It.Is<Person>(p => p == person)), Times.Once());
+            _repositoryMock.Verify(m => m.Update(It.Is<Person>(p => p == invalidPerson)), Times.Never());
+        }
+
+        [Test]
+        public void Delete_ShouldCallRepositoryDeleteOnce_Test()
+        {
+            // Arrange
+            const int id = 4;
+            const string name = "George";
+            const int age = 26;
+            var person = new Person
+            {
+                Id = id,
+                Name = name,
+                Age = age
+            };
+            var invalidPerson = new Person();
+            _repositoryMock.Setup(m => m.Delete(It.Is<Person>(p => p == person)))
+                .Verifiable();
+            var service = new PersonService(_repositoryMock.Object);
+
+            // Act
+            service.Delete(person);
+
+            // Assert
+            _repositoryMock.Verify(m => m.Delete(It.Is<Person>(p => p == person)), Times.Once());
+            _repositoryMock.Verify(m => m.Delete(It.Is<Person>(p => p == invalidPerson)), Times.Never());
         }
     }
 }
