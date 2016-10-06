c256dd4dcace1f1e783e03fa26c8a76f52e943f4
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-10-06 21:58:57 +0100

    Fixed autofac types in container

--- People.SelfHostedApi/App_Start/AutofacConfig.cs
+++ People.SelfHostedApi/App_Start/AutofacConfig.cs
@@ -4,6 +4,9 @@
     using System.Web.Http;
     using Autofac;
     using Autofac.Integration.WebApi;
+    using Domain.Context;
+    using Domain.Entities;
+    using Domain.Repository;
     using Services.Person;
     using Owin;
 
@@ -13,6 +16,8 @@
         {
             var builder = new ContainerBuilder();
             builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterType<ApplicationDbContext>().As<BaseDbContext>();
+            builder.RegisterType<Repository<Person, int>>().As<IRepository<Person, int>>();
             builder.RegisterType<PersonService>().As<IPersonService>().InstancePerRequest();
 
             var container = builder.Build();
