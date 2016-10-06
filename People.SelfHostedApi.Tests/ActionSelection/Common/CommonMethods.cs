b14d4f2c02ed1beeaea33640d71be301cf445eff
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-10-06 23:41:34 +0100

    Fixed action selection tests

---
+++ People.SelfHostedApi.Tests/ActionSelection/Common/CommonMethods.cs
@@ -0,0 +1,19 @@
+﻿namespace People.SelfHostedApi.Tests.ActionSelection.Common
+{
+    using System.Net.Http;
+    using System.Web.Http;
+    using System.Web.Http.Hosting;
+    using System.Web.Http.Routing;
+
+    public class CommonMethods
+    {
+        public static HttpRequestMessage SetupRequest(string url, string method, HttpConfiguration config, out IHttpRouteData routeData)
+        {
+            var request = new HttpRequestMessage(new HttpMethod(method), url);
+            routeData = config.Routes.GetRouteData(request);
+            request.Properties[HttpPropertyKeys.HttpRouteDataKey] = routeData;
+            request.Properties[HttpPropertyKeys.HttpConfigurationKey] = config;
+            return request;
+        }
+    }
+}
--- People.SelfHostedApi.Tests/ActionSelection/UserController/UserControllerActionSelectionTests.cs
+++ People.SelfHostedApi.Tests/ActionSelection/UserController/UserControllerActionSelectionTests.cs
@@ -1,15 +1,40 @@
 ﻿namespace People.SelfHostedApi.Tests.ActionSelection.UserController
 {
+    using System;
+    using System.Web.Http;
+    using System.Web.Http.Controllers;
+    using System.Web.Http.Routing;
+    using Common;
     using NUnit.Framework;
+    using SelfHostedApi.Controllers;
 
     [TestFixture]
     public class UserControllerActionSelectionTests
     {
         [Test]
-        public void TestMethod()
+        [TestCase("http://localhost:3001/api/user/", "GET", typeof(UserController), "Get")]
+        public void CorrectControllerAndActionAreSelected_Test(string url, string method, Type controller, string action)
         {
-            // TODO: Add your test code here
-            Assert.Pass("Your first passing test");
+            // Arrange
+            IHttpRouteData routeData;
+            var config = new HttpConfiguration();
+            WebApiRouteConfig.Register(config);
+            config.EnsureInitialized();
+            var actionSelector = config.Services.GetActionSelector();
+            var controllerSelector = config.Services.GetHttpControllerSelector();
+            var request = CommonMethods.SetupRequest(url, method, config, out routeData);
+
+            // Act
+            var controllerDescriptor = controllerSelector.SelectController(request);
+            var context = new HttpControllerContext(config, routeData, request)
+            {
+                ControllerDescriptor = controllerDescriptor
+            };
+            var actionDescriptor = actionSelector.SelectAction(context);
+
+            // Assert
+            Assert.AreEqual(controller, controllerDescriptor.ControllerType);
+            Assert.AreEqual(action, actionDescriptor.ActionName);
         }
     }
 }
