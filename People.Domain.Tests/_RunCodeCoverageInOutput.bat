51dc00fd6da0076cbe21ac1aaa315761113370de
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-10-09 14:17:52 +0100

    Added filters to exclude trivial code from coverage

--- People.Domain.Tests/_RunCodeCoverageInOutput.bat
+++ People.Domain.Tests/_RunCodeCoverageInOutput.bat
@@ -1,4 +1,4 @@
-"..\..\..\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe" -target:"..\..\..\packages\NUnit.ConsoleRunner.3.4.1\tools\nunit3-console.exe" -targetargs:"People.Domain.Tests.dll" -filter:"+[People.Domain]People.Domain*" -excludebyattribute:"System.CodeDom.Compiler.GeneratedCodeAttribute" -register:user -output:"_CodeCoverageResult.xml"
+"..\..\..\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe" -target:"..\..\..\packages\NUnit.ConsoleRunner.3.4.1\tools\nunit3-console.exe" -targetargs:"People.Domain.Tests.dll" -filter:"+[People.Domain]People.Domain* -[*]People.Domain.Migrations* -[*]People.Domain.Context.BaseDbContext* -[*]People.Domain.Entities*" -excludebyattribute:"System.CodeDom.Compiler.GeneratedCodeAttribute" -register:user -output:"_CodeCoverageResult.xml"
 @pause
 
 "..\..\..\packages\ReportGenerator.2.4.5.0\tools\ReportGenerator.exe" "-reports:_CodeCoverageResult.xml" "-targetdir:_CodeCoverageReport"
