eb69f070db2495df65f7533b4ff984b37dfa5b89
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-11-01 22:00:01 +0000

    Refactored repository tests

--- People.Domain.Tests/Repository/RepositoryTests.cs
+++ People.Domain.Tests/Repository/RepositoryTests.cs
@@ -1,8 +1,10 @@
 ﻿namespace People.Domain.Tests.Repository
 {
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
+    using System.Linq;
     using Domain.Context;
     using Domain.Repository;
     using Moq;
@@ -32,18 +34,81 @@
         }
 
         [Test]
+        public void All_ReturnsAnIQueryableOfEntityStub_Test()
+        {
+            // Arrange
+            var entityStubs = SetupEntityStubsQueryable();
+            var dbSetMock = GetDbSetMock();
+            SetupIQueryable(dbSetMock, entityStubs);
+            _dbContextMock.Setup(s => s.GetDbSet<EntityStub>()).Returns(dbSetMock.Object);
+            var repository = CreateRepository();
+
+            // Act
+            var result = repository.All.ToList();
+
+            // Assert
+            IsNotEmpty(result);
+            AreEqual(3, result.Count);
+        }
+
+        private static IQueryable<EntityStub> SetupEntityStubsQueryable()
+        {
+            var entityStubs = new List<EntityStub>
+            {
+                new EntityStub(),
+                new EntityStub(),
+                new EntityStub()
+            }.AsQueryable();
+            return entityStubs;
+        }
+
+        private static Mock<IDbSet<EntityStub>> GetDbSetMock()
+        {
+            var dbSetMock = new Mock<IDbSet<EntityStub>>();
+            return dbSetMock;
+        }
+
+        private static void SetupIQueryable(Mock<IDbSet<EntityStub>> dbSetMock, IQueryable<EntityStub> entityStubs)
+        {
+            dbSetMock.Setup(s => s.Provider).Returns(entityStubs.Provider);
+            dbSetMock.Setup(s => s.Expression).Returns(entityStubs.Expression);
+            dbSetMock.Setup(s => s.ElementType).Returns(entityStubs.ElementType);
+        }
+
+        private Repository<EntityStub, string> CreateRepository()
+        {
+            return new Repository<EntityStub, string>(_dbContextMock.Object);
+        }
+
+        [Test]
+        public void All_ReturnsAnEmptyCollection_Test()
+        {
+            // Arrange
+            var expected = Enumerable.Empty<EntityStub>().AsQueryable();
+            var dbSetMock = GetDbSetMock();
+            SetupIQueryable(dbSetMock, expected);
+            _dbContextMock.Setup(s => s.GetDbSet<EntityStub>()).Returns(dbSetMock.Object);
+            var repository = CreateRepository();
+
+            // Act
+            var result = repository.All;
+
+            // Assert
+            IsEmpty(result);
+        }
+
+        [Test]
         public void Find_ForGivenKey_ReturnsAnEntityStubInstance()
         {
             // Arrange
             const string key = "123";
             const string nonExistentKey = "1234";
             var entityStub = new EntityStub();
-            var dbSetMock = new Mock<IDbSet<EntityStub>>();
+            var dbSetMock = GetDbSetMock();
             dbSetMock.Setup(m => m.Find(It.Is<string>(s => s == key))).Returns(entityStub);
             dbSetMock.Setup(m => m.Find(It.Is<string>(s => s == nonExistentKey))).Returns(() => null);
-
             _dbContextMock.Setup(s => s.GetDbSet<EntityStub>()).Returns(() => dbSetMock.Object);
-            var repository = new Repository<EntityStub, string>(_dbContextMock.Object);
+            var repository = CreateRepository();
 
             // Act
             var nullResult = repository.Find(nonExistentKey);
@@ -61,7 +126,7 @@
             // Arrange
             _dbContextMock.Setup(m => m.Entry(It.IsAny<EntityStub>(), It.IsAny<Action<DbEntityEntry<EntityStub>>>())).Verifiable();
             _dbContextMock.Setup(m => m.SaveChanges()).Verifiable();
-            var repository = new Repository<EntityStub,string>(_dbContextMock.Object);
+            var repository = CreateRepository();
 
             // Act
             repository.Update(new EntityStub());
@@ -77,7 +142,7 @@
             // Arrange
             _dbContextMock.Setup(m => m.Entry(It.IsAny<EntityStub>(), It.IsAny<Action<DbEntityEntry<EntityStub>>>())).Verifiable();
             _dbContextMock.Setup(m => m.SaveChanges()).Verifiable();
-            var repository = new Repository<EntityStub, string>(_dbContextMock.Object);
+            var repository = CreateRepository();
 
             // Act
             repository.Delete(new EntityStub());
@@ -88,11 +153,12 @@
         }
 
         [Test]
-        public void Create_EntryWasCalledOnce_SaveChangesWasCalledOnce_Test() {
+        public void Create_EntryWasCalledOnce_SaveChangesWasCalledOnce_Test()
+        {
             // Arrange
             _dbContextMock.Setup(m => m.Entry(It.IsAny<EntityStub>(), It.IsAny<Action<DbEntityEntry<EntityStub>>>())).Verifiable();
             _dbContextMock.Setup(m => m.SaveChanges()).Verifiable();
-            var repository = new Repository<EntityStub, string>(_dbContextMock.Object);
+            var repository = CreateRepository();
 
             // Act
             repository.Create(new EntityStub());
